def72e32c5936a3efce3f16210593054dffc9bad
Glatorian13 <glatorian13@gmail.com>
2021-05-20 20:59:41 -0400

    react changes

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -3,33 +3,25 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    NavLinks
   } from "react-router-dom";
 import "./style.css";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 export default function Footer() {
   return (
       <Router>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand nav-brand-custom" href=".">Javier</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href=".">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=".">Portfolio</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href=".">About<span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <Navbar expand="lg" variant="light" bg="light">
+        <a class="navbar-brand nav-brand-custom" href="/">Javier</a>
+        <NavDropdown title="Additional Links" id="navbarNav">
+            <Nav className="navbar-nav ml-auto">
+                <NavDropdown.Item href="/Contact">Contact</NavDropdown.Item>
+                <NavDropdown.Item href="/Portfolio">Portfolio</NavDropdown.Item>
+                <NavDropdown.Item href="/About">About</NavDropdown.Item>
+            </Nav>
+        </NavDropdown>
+    </Navbar>
     </Router>
   );
 }
